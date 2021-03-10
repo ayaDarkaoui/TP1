64aa8218bb7dc2c5b8707dc34f9f1f5ac3e8a4c1
ayaDarkaoui <80391001+ayaDarkaoui@users.noreply.github.com>
2021-03-10 22:46:47 +0100

    Add files via upload

---
+++ Mad.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtelierN
+{
+    class MAD
+    {
+        private  double valeur;
+        
+
+        public MAD(double s)
+        {
+            this.valeur = s;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this.valeur+" MAD");
+        }
+
+       
+        public static bool operator >(MAD x, MAD y)
+        {
+            if (x.valeur > y.valeur) return true;
+
+            return false;
+        }
+
+        public static bool operator <(MAD x, MAD y)
+        {
+            if (x.valeur < y.valeur) return true;
+
+            return false;
+        }
+        public static MAD operator -(MAD x, MAD y)
+        {
+            return new MAD(x.valeur - y.valeur);
+        }
+        public static MAD operator +(MAD x, MAD y)
+        {
+            return  new MAD(x.valeur + y.valeur);
+        }
+
+    }
+}
---
+++ Programs (1).cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtelierN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            Client cl1 = new Client("darkaoui", "aya", "boulvard quods");
+            Client cl2 = new Client("wisal", "chtmi", "les orangers");
+        
+            Comptes A = new Comptes(new MAD(2800), cl1, new MAD(3500));
+            Comptes B = new Comptes(new MAD(8900) , cl2, new MAD(1200));
+
+            A.Consulter();
+            B.Consulter();
+
+            A.Verser(B, new MAD(1500));
+            Console.WriteLine("\n         Apres les virement:\n");
+           
+            A.Consulter();
+            B.Consulter();
+
+            Console.ReadKey();
+
+        }
+    }
+}
